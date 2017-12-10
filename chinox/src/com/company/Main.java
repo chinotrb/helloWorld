0cbbd404f87f47a7f6855323e20e80cf197d6393
chinotrb <chinotrb@gmail.com>
2017-12-10 16:27:01 -0600

    perimetro y area

--- chinox/src/com/company/Main.java
+++ chinox/src/com/company/Main.java
@@ -5,15 +5,35 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-     	Scanner teclado = new Scanner(System.in);
-        
-        System.out.print("escriba el primer numero");        
+        PerimetroAreaDelCuadrado();
+    }
+
+    public static void PerimetroAreaDelCuadrado() {
+        Scanner teclado  = new Scanner(System.in);
+
+        System.out.print("Digite la medida del lado: ");
+        int medidaLado = teclado.nextInt();
+
+        System.out.print("el perimetro es: ");
+        System.out.println(medidaLado * 4);
+
+        System.out.print("EL area es:");
+        System.out.print(medidaLado * medidaLado);
+
+
+    }
+
+    public static void SumaDeNumeros(){
+        Scanner teclado = new Scanner(System.in);
+
+        System.out.print("escriba el primer numero");
         int primerNumero = teclado.nextInt();
 
-        System.out.print("escriba el segundo numero");        
+        System.out.print("escriba el segundo numero");
         int segundoNumero = teclado.nextInt();
 
         System.out.print("la suma de los numeros es:");
         System.out.print(primerNumero + segundoNumero);
+
     }
 }
