aa1715f9f52b9712ed443049e7078fe894b0e089
chinotrb <chinotrb@gmail.com>
2017-12-10 17:30:26 -0600

    perimetro y area

--- chinox/src/com/company/Main.java
+++ chinox/src/com/company/Main.java
@@ -5,20 +5,8 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        PerimetroAreaDelCuadrado();
-    }
+        SumaDeNumeros();
 
-    public static void PerimetroAreaDelCuadrado() {
-        Scanner teclado  = new Scanner(System.in);
-
-        System.out.print("Digite la medida del lado: ");
-        int medidaLado = teclado.nextInt();
-
-        System.out.print("el perimetro es: ");
-        System.out.println(medidaLado * 4);
-
-        System.out.print("EL area es:");
-        System.out.print(medidaLado * medidaLado);
 
 
     }
@@ -26,14 +14,31 @@ public class Main {
     public static void SumaDeNumeros(){
         Scanner teclado = new Scanner(System.in);
 
-        System.out.print("escriba el primer numero");
+        System.out.print("escriba el primer numero: ");
         int primerNumero = teclado.nextInt();
 
-        System.out.print("escriba el segundo numero");
+        System.out.print("escriba el segundo numero: ");
         int segundoNumero = teclado.nextInt();
 
-        System.out.print("la suma de los numeros es:");
-        System.out.print(primerNumero + segundoNumero);
+        System.out.print("la suma de los numeros es: ");
+        System.out.println(primerNumero + segundoNumero);
 
-    }
-}
+        System.out.print("Escriba un numero: ");
+        int terserNumero = teclado.nextInt();
+
+        System.out.print("escriba un numero: ");
+        int cuartoNumero = teclado.nextInt();
+
+        System.out.print("la multiplicasion del terser y cuarto numero es: ");
+        System.out.print(terserNumero * cuartoNumero);
+
+
+
+    }}
+
+
+
+
+
+
+
