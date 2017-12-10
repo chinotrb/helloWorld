c8a23e92e65b41d55351c0079249fd0652b320bb
chinotrb <34425230+chinotrb@users.noreply.github.com>
2017-12-10 15:31:45 -0600

    Fixes

--- chinox/src/com/company/Main.java
+++ chinox/src/com/company/Main.java
@@ -5,13 +5,13 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-     	System.out.print("escriba el primer numero");
-        Scanner teclado = new Scanner(System.in);
+     	Scanner teclado = new Scanner(System.in);
+        
+        System.out.print("escriba el primer numero");        
         int numero = teclado.nextInt();
 
-        System.out.print("escriba el segundo numero");
-        Scanner teclado2 = new Scanner(System.in);
-        int dijito = teclado2.nextInt();
+        System.out.print("escriba el segundo numero");        
+        int dijito = teclado.nextInt();
 
         System.out.print("la suma de los numeros es:");
         System.out.print(numero + dijito);
